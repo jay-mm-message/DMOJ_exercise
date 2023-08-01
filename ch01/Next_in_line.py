8b47e1f704aacc67f09f824f1141ba0438d1e1c3
jay <jay.mm.message@gmail.com>
2023-08-01 09:25:13 +0800

    Next in Line

---
+++ ch01/Next_in_line.py
@@ -0,0 +1,9 @@
+# DMOJ problem ccc13j1, Next in Line
+
+youngest_child_age = int(input())
+middle_child_age = int(input())
+
+
+oldest_child_age = middle_child_age * 2 - youngest_child_age
+
+print(oldest_child_age)
