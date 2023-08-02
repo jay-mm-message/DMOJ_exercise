a029f8ea63ce2fc27556c6ebc7dae80fbd0c51a4
jay <jay.mm.message@gmail.com>
2023-08-02 12:16:31 +0800

    Special Day

---
+++ ch02/Special_Day.py
@@ -0,0 +1,16 @@
+# DMOJ problem ccc15j1, Special Day
+
+month = int(input())
+day = int(input())
+
+if month > 2:
+    print('After')
+elif month < 2:
+    print('Before')
+else:
+    if day > 18:
+        print('After')
+    elif day < 18:
+        print('Before')
+    else:
+        print('Special')
