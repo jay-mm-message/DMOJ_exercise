072ca69f0e916dafa36ea789d141b82f23bea3e8
jay <jay.mm.message@gmail.com>
2023-08-15 15:34:10 +0800

    refactor

--- ch04/SlotMachines2.py
+++ ch04/SlotMachines2.py
@@ -1,3 +1,5 @@
+# This is DMOJ problem ccc00s1.
+
 quarters = int(input())
 first = int(input())
 second = int(input())
@@ -9,23 +11,29 @@ third = int(input())
 machine = 0
 slot_time = 0
 while quarters > 0:
-    if machine % 3 == 0:
+    if (machine % 3) == 0:
         first = first + 1
-        if first == 35:
-            first = 0
+        if (first % 35) == 0:
             quarters = quarters + 30
+        # if first == 35:
+        #     first = 0
+        #     quarters = quarters + 30
 
-    elif machine % 3 == 1:
+    elif (machine % 3) == 1:
         second = second + 1
-        if second == 100:
-            second = 0
+        if (second % 100) == 0:
             quarters = quarters + 60
+        # if second == 100:
+        #     second = 0
+        #     quarters = quarters + 60
 
-    elif machine % 3 == 2:
+    elif (machine % 3) == 2:
         third = third + 1
-        if third == 10:
-            third = 0
+        if (third % 10) == 0:
             quarters = quarters + 9
+        # if third == 10:
+        #     third = 0
+        #     quarters = quarters + 9
 
     slot_time = slot_time + 1
     machine = machine + 1 
